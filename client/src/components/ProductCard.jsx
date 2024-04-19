6cf8ef1c38a7a5d45fddd0addd25083b437e0c7f
ayaaz <polkadotboom@protonmail.com>
2024-04-20 03:31:34 +0530

    end of chp2

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,8 +1,16 @@
 import { Box, Image, Text, Badge, Flex, IconButton, Skeleton } from '@chakra-ui/react';
 import { BiExpand } from 'react-icons/bi';
 import React from 'react';
+import { addToFavorites, removeFromFavorites } from '../redux/actions/productActions';
+import { useSelector, useDispatch } from 'react-redux';
+import { MdOutlineFavorite, MdOutlineFavoriteBorder } from 'react-icons/md';
+
 
 const ProductCard = ({ product, loading }) => {
+    const dispatch = useDispatch();
+    const { favorites } = useSelector((state )=> state.product);
+
+
     return (
         <Skeleton isLoaded={!loading} _hover={{ size: 1.5 }}>
             <Box
@@ -30,17 +38,38 @@ const ProductCard = ({ product, loading }) => {
                 <Text noOfLines={1} fontSize='md' color='gray.600'>
                     {product.subtitle}
                 </Text>
-               <flex justify='space-between' alignItems='center' mt='2'>
+               <Flex justify='space-between' alignItems='center' mt='2'>
                 <Badge colorScheme='cyan'>{product.category}</Badge>
                     <Text fontSize='xl' fontweight='semibold' color='cyan.600'>
                         ₹{product.price}
                     </Text>
-               </flex>
-               <IconButton icon={<BiExpand size='20'/> 
-               } colorScheme='cyan' size='sm'/>
+               </Flex>
+               <Flex justify='space-between' mt='2'>
+					{favorites.includes(product._id) ? (
+						<IconButton
+							icon={<MdOutlineFavorite size='20px' />}
+							colorScheme='cyan'
+							size='sm'
+							onClick={() => dispatch(removeFromFavorites(product._id))}
+						/>
+					) : (
+						<IconButton
+							icon={<MdOutlineFavoriteBorder size='20px' />}
+							colorScheme='cyan'
+							size='sm'
+							onClick={() => dispatch(addToFavorites(product._id))}
+						/>
+					)}
+
+					<IconButton icon={<BiExpand size='20' />} colorScheme='cyan' size='sm' />
+				</Flex>
+            
             </Box>
         </Skeleton>
     );
 };
 
 export default ProductCard;
+
+
+
